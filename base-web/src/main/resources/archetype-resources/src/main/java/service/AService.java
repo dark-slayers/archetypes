241fa9acaeeb6bbc3d9d969e11e47eeaf5272cd4
刘湘湘 <liuxx@hyc.cn>
2017-03-15 19:26:06 +0800

    修改Service模板类，增加导入

--- base-web/src/main/resources/archetype-resources/src/main/java/service/AService.java
+++ base-web/src/main/resources/archetype-resources/src/main/java/service/AService.java
@@ -1,9 +1,20 @@
 package ${package}.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+import ${package}.dao.Dao;
+import ${package}.vo.Item;
+
 @Service("aService")
 @Scope("prototype")
 public class AService
 {
 	@Autowired
 	private Dao dao;
+
+	public void s()
+	{
+		dao.search(Item.class, 1);
+	}
 }
