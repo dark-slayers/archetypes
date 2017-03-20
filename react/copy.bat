9e90bd9937d09d2142deb9e569cc5144b2bd54fd
刘湘湘 <liuxx@hyc.cn>
2017-03-20 19:06:59 +0800

    增加一个react基本项目框架

---
+++ react/copy.bat
@@ -0,0 +1,4 @@
+rd  E:\GitProject\mrp-util\src\main\webapp\packpage /S/Q
+del E:\GitProject\mrp-util-page\packpage\vendor.bundle.js
+move E:\GitProject\mrp-util-page\vendor.bundle.js E:\GitProject\mrp-util-page\packpage\
+xcopy E:\GitProject\mrp-util-page\packpage E:\GitProject\mrp-util\src\main\webapp\packpage /E/I
---
+++ react/webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require("webpack");
+var config = {
+  entry: {
+    vendor: ["jquery", "react", 'react-dom'],
+    "/packpage/debug": "./src/entry/debug.entry.js",
+  },
+  output: {
+    path: './',
+    filename: "[name].bundle.js",
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' })
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015','stage-0', 'react'],
+      }
+    }, {
+       test: /\.css$/,
+       loader: "style-loader!css-loader"
+     }]
+  }
+}
+module.exports = config;
