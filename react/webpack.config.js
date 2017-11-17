5cdd6fcb6115adbecd008adc95b0d318f6a5ae0f
dark-slayers <23534878@qq.com>
2017-11-17 17:24:22 +0800

    修改react项目模板配置文件

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,5 +1,22 @@
 var webpack = require("webpack");
 var path = require('path');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 var config = require('./webpack.base.config.js');
 config.module.rules[0].use[0].options.plugins.push('transform-remove-console');
+config.plugins.push(new UglifyJSPlugin({
+  uglifyOptions: {
+    ie8: false,
+    output: {
+      comments: false,
+      beautify: false,
+    },
+    mangle: {
+      keep_fnames: true
+    },
+    compress: {
+      warnings: false,
+      drop_console: true
+    },
+  }
+}));
 module.exports = config;
